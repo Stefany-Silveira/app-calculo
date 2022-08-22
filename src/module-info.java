76979174d493b057d6ddc18759fe0d1e250413b1
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-22 15:46:51 -0300

    add @ Exports To

--- src/module-info.java
+++ src/module-info.java
@@ -3,4 +3,6 @@ module app.calculo {
     requires transitive app.logging;
     exports br.com.stefany.app.calculo;
 
+    exports br.com.stefany.app.calculo.interno
+            to app.financeiro;
 }

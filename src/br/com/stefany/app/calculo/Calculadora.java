a9cc0828942fff478902c5ddc650cbedcd8b880c
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-22 15:42:13 -0300

    add @ Requires Transitive

--- src/br/com/stefany/app/calculo/Calculadora.java
+++ src/br/com/stefany/app/calculo/Calculadora.java
@@ -1,12 +1,14 @@
 package br.com.stefany.app.calculo;
 
 import br.com.stefany.app.calculo.interno.OperacoesAritmeticas;
+import br.com.stefany.app.logging.Logger;
 
 public class Calculadora {
 
     private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
 
     public double soma(double... nums) {
+        Logger.info("Somando...");
         return opAritmeticas.soma(nums);
     }
 }
